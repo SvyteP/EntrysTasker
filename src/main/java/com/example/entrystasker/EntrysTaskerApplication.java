d892be83d2eee5bd99cf7fe685350efecc8b68bc
Svyter <Max161161161@mail.ru>
2023-11-24 20:05:29 +0300

    Initial commit

---
+++ src/main/java/com/example/entrystasker/EntrysTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrystasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrysTaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrysTaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/entrystasker/EntrysTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrystasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrysTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
